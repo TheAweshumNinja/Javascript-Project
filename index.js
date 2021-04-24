aebfbc3dec36cd588e579ae35d3afd602e214f69
Samantha <samanthademotta@yahoo.com>
2021-04-24 15:41:57 -0400

    redo html and js

--- index.js
+++ index.js
@@ -1,30 +1,4 @@
-const getFilms= () =>{
-    return fetch('https://ghibliapi.herokuapp.com/films')
-           .then(res => res.json())
-}
 
 
-const createFilmDiv = (film) =>{
-    const filmContainer= document.createElement('div')
-    const title= document.createElement('p')
-    const description = document.createElement('p')
-    const originalTitle = document.createElement('p')
-    const species = document.createElement('p')
-    const location = document.createElement('p')
-    const searchBtn= document.createElement('button')
-
-    title.innerText= film.title
-    description.innerText= film.description
-    filmContainer.append(title, description, originalTitle, species, location, searchBtn)
-    return filmContainer
-}
-getFilms().then((film) => {
-    const filmDiv = createFilmDiv(film)
-    console.log(filmDiv)
-
-})
-document.getElementById("search-button").addEventListener("click", myFunction);
-
-function myFunction() {
-  document.getElementById("search-button").innerHTML = "Search";
-}
+const buttonElement = document.querySelector('#search-button');
+const inputElement = document.querySelector('#inputValue');
