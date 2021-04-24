4b10fe86b83d3f54859a8dc44f625fdd92bc9c54
Samantha <samanthademotta@yahoo.com>
2021-04-24 16:03:02 -0400

    work on event listener

--- index.js
+++ index.js
@@ -2,3 +2,14 @@
 
 const buttonElement = document.querySelector('#search-button');
 const inputElement = document.querySelector('#inputValue');
+
+
+
+
+
+buttonElement.onclick = function(event) {
+    event.preventDefault();
+    const value = inputElement.value;
+    console.log(' Value: ', value);
+    console.log('Hello World, this button has been clicked ')
+}
